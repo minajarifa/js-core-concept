fc5d1d688bb34f6397e62fb2725d1d86678dfa86
Your Name <minajmiha02@gmail.com>
2026-02-06 20:11:00 +0600

    nonprimitive

--- data-type.js
+++ data-type.js
@@ -1,10 +1,22 @@
+// primitive data type
 let data = 23;
-data=false
-data={
-    name: "aru",
-    price: 6000
-}
-data=[12,13,14,15,16,17,18]
-data=undefined
-data=null
+data = false;
+data = {
+  name: "aru",
+  price: 6000,
+};
+data = [12, 13, 14, 15, 16, 17, 18];
+data = undefined;
+data = null;
 console.log(typeof data);
+
+// nonprimitive
+const price = 50;
+const name = "abul";
+const isPoor = false;
+const student = {
+  name: "babul",
+  location: "lalbag",
+  subject: "CSC",
+};
+const friend = ["tom", "bob", "jeck", "rock"];
