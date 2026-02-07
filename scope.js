5c321714aa59b727e785c7ed5eb0bb8ab439e6ae
Your Name <minajmiha02@gmail.com>
2026-02-07 21:27:44 +0600

    ses

--- scope.js
+++ scope.js
@@ -1,5 +1,14 @@
 if (true) {
   const data = 54;
-  console.log(data);
-  console.log(data);
+  //   console.log(data);
+}
+
+for (const num of [1, 2, 3, 4, 5, 6, 7]) {
+  //   console.log(num);
+}
+// bahit theke 
+function doMath(a, b) {
+  console.log(a, b);
+  const sum = a + b;
+  const total = sum + 10;
 }
